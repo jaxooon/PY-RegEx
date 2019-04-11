77f5925bd26bc5abf9ae454036108a799cbcd2a7
Jackson Barr <jbarr@brocksolutions.com>
2019-04-11 15:47:46 -0400

    RegEx altered to find and replace anti-cascade rungs with HMI-overide powered anti-cascade rungs

---
+++ HMI OVERRIDE ANTI CASCADE/PY_ANTI_CASCADE.py
@@ -0,0 +1,29 @@
+import glob
+import re
+
+FileNameList = []
+FileNameListLength = 0
+
+for filename in glob.glob('*.L5*'):
+    FileNameList.append(filename)
+
+print(FileNameList)
+
+FileNameListLength = len(FileNameList)
+
+print(FileNameListLength)
+
+
+for name in FileNameList:
+    with open(name, 'r') as TextFile:
+        WordsWritten = [line.strip() for line in TextFile]
+
+    lines = [ re.sub(r"(<!\[CDATA\[)(XIC\(F_)([A-Z][1-9][A-Z]_|[A-Z][A-Z][1-9]_|[A-Z][A-Z][1-9][1-9][A-Z]_|[A-Z][A-Z][1-9][A-Z]_)([0-9][0-9]|[0-9])(_MSD_OFF\))(OTE\()([A-Z][1-9][A-Z]_|[A-Z][A-Z][1-9]_|[A-Z][A-Z][1-9][1-9][A-Z]_|[A-Z][A-Z][1-9][A-Z]_)([0-9][0-9]|[0-9])(_MTR.ANTI_CASC\);]]>)", 
+            r"\1[\2\3\4\5, XIC(F_\3\4_OVRIDE)]\6\7\8\9", 
+            line) for line in WordsWritten ]
+    
+    
+
+    with open(name, 'w') as TextFile:
+        TextFile.write('\n'.join(lines))
+
